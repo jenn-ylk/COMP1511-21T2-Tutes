544bfb68ccaf1042529faad222722e98f1d9015c
Jennifer <jennifer.king1@student.unsw.edu.au>
2021-06-04 14:35:47 +1000

    thursday exercises

---
+++ thu/wk01/face.c
@@ -0,0 +1,18 @@
+// face.c, draws a face and prints it to the terminal
+// face0:
+// ~ ~
+// 0 0
+//  o
+//  -
+// Jenny King, Thursday 4pm tute, 3/6/21
+
+#include <stdio.h>
+
+int main(void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf("\\_/\n");
+
+    return 0;
+}
